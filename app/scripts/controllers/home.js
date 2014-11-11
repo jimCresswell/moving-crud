5a8543549f910d852342834bb4b432f5d8d081b2
jimCresswell <jim.cresswell@gmail.com>
2014-11-11 16:15:00 +0000

    renamed main module to home module

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moving-crud.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the moving-crud
+ */
+angular.module('moving-crud')
+  .controller('HomeCtrl', HomeController);
+
+function HomeController() {
+    var vm = this;
+
+    vm.items = [
+      'Item 1',
+      'Item 2',
+      'Item 4'
+    ];
+
+    vm.processItem = processItem;
+    vm.removeItem = removeItem;
+
+    function processItem() {
+
+        // Reject falsy item descriptions including undefined and empty string.
+        if (!vm.newItem) {
+            return;
+        }
+
+        // Verify intentional duplicate items
+        if (vm.items.indexOf(vm.newItem) !== -1) {
+            if (!window.confirm('Are you sure you want to add the same item again?')) {
+                return;
+            }
+        }
+
+        addItem(vm.newItem);
+        vm.newItem = '';
+    };
+
+    function addItem(item) {
+        vm.items.push(item);
+    }
+
+    function removeItem(index) {
+        vm.items.splice(index, 1);
+    }
+}
