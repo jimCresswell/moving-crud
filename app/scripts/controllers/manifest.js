e18970718cd14c2d60bfe05bf67632aa1e27f732
jimCresswell <jim.cresswell@gmail.com>
2014-11-11 23:24:46 +0000

    Basic output for manifest page

--- app/scripts/controllers/manifest.js
+++ app/scripts/controllers/manifest.js
@@ -8,11 +8,12 @@
  * Controller of the moving-crud
  */
 angular.module('moving-crud')
-  .controller('ManifestCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('ManifestCtrl', ManifestController);
 
+ManifestController.$inject = ['localStorageService'];
+
+function ManifestController(localStorageService) {
+    var vm = this;
+
+    vm.rooms = localStorageService.get('rooms') || [];
+}
